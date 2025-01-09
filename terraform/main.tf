c6261b756ff7345f2d149336c4513bd12f312b3a
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-01-09 09:20:22 -0600

    Enhance Azure authentication in Terraform configuration and deployment workflow
    - Added Azure authentication parameters (subscription_id, tenant_id, client_id, client_secret) to the Terraform provider configuration for improved security and clarity.
    - Updated the GitHub Actions workflow to include these authentication parameters in the backend configuration, ensuring a more robust deployment process.
    
    These changes strengthen the integration with Azure and enhance the maintainability of the Terraform setup.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,11 @@
 # Configure Azure Provider
 provider "azurerm" {
   features {}
+  
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 # Create Resource Group
